59693f904ba02581743ba5a65a2d24b58c71a9c5
Sahil Mansoori <sahil.mansoori@tomiaglobal.com>
2021-11-18 00:33:33 +0530

    Prod service first commit

---
+++ ProductMicroservice/Repository/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using ProductMicroservice.Models;
+
+namespace ProductMicroservice.Repository
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+        Product GetProductByID(int productId);
+        void InsertProduct(Product product);
+        void DeleteProduct(int productId);
+        void UpdateProduct(Product product);
+        void Save();
+    }
+}
---
+++ ProductMicroservice/Repository/ProductRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductMicroservice.Models;
+using ProductMicroservice.DBContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductMicroservice.Repository
+{
+    public class ProductRepository: IProductRepository
+    {
+        private readonly ProductContext _dbContext;
+
+        public ProductRepository(ProductContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _dbContext.Products.ToList();
+        }
+
+        public Product GetProductByID(int productId)
+        {
+            return _dbContext.Products.Find(productId);
+        }
+        public void DeleteProduct(int productId)
+        {
+            var product = _dbContext.Products.Find(productId);
+            _dbContext.Products.Remove(product);
+            Save();
+
+        }
+        public void InsertProduct(Product product)
+        {
+            _dbContext.Add(product);
+            Save();
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _dbContext.Entry(product).State = EntityState.Modified;
+            Save();
+        }
+    }
+}
